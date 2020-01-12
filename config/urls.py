be2b070845c777fe0e0bfe88872af048ee307892
hrtg-yamazaki <hrtg.yamazaki@gmail.com>
2020-01-12 22:29:22 +0900

    SCSSの適用
    ・scssファイルのテンプレートファイルへの適用を可能にしました。
    ・urls.pyを修正しました。

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('', include('practice.urls')),
     path('admin/', admin.site.urls),
+    path('practice/', include('practice.urls')),
+    path('', RedirectView.as_view(url='/practice/'))
 ]
