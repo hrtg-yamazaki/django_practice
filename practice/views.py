340e74482303f983577e326419706ccdcd2a1d79
hrtg-yamazaki <hrtg.yamazaki@gmail.com>
2020-01-14 13:54:48 +0900

    投稿ユーザーの自動反映
    ・どのユーザーが記事を投稿/編集したのかをフォームでの選択なしに自動で反映できるよう、ビューを修正しました。

--- practice/views.py
+++ practice/views.py
@@ -3,6 +3,7 @@ from django.views import generic
 from .models import Article
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
 
 class IndexView(generic.ListView):
     model = Article
@@ -12,11 +13,21 @@ class DetailView(generic.DetailView):
 
 class CreateView(LoginRequiredMixin, generic.edit.CreateView):
     model = Article
-    fields = '__all__'
+    fields = ['title', 'content', 'category']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super(CreateView, self).form_valid(form)
 
 class UpdateView(LoginRequiredMixin, generic.edit.UpdateView):
     model = Article
-    fields = '__all__'
+    fields = ['title', 'content', 'category']
+
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.author != self.request.user:
+            raise PermissionDenied('編集権限がありません')
+        return super(UpdateView, self).dispatch(request, *args, **kwargs)
 
 class DeleteView(LoginRequiredMixin, generic.edit.DeleteView):
     model = Article
